881137ad1342c8e6b925f4ed142c3a4b58abcd0e
Nick Frattaroli <Nafratt@gmail.com>
2019-12-21 13:10:01 -0500

    adding thumbnails

--- client/src/Components/score.jsx
+++ client/src/Components/score.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 export default function Score(props){
-    const styles= {
-        width:'200px'
-    };
+
     
     return(<div> {props.score}|top Score:{props.topScore}
         </div>
