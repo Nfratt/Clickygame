c0e7b3b8fec99c1a46400140473e2b22eb081672
Nick Frattaroli <Nafratt@gmail.com>
2019-12-21 12:30:29 -0500

    finalizing score.jsx

--- client/src/Components/score.jsx
+++ client/src/Components/score.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 export default function Score(props){
     const styles= {
-
+        width:'200px'
     };
-    return (
-        <div
-    )
+    
+    return(<div> {props.score}|top Score:{props.topScore}
+        </div>
+    );
 }
